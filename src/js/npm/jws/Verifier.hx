8798c2960016f48d83ef2508f80b3ddd26f4c148
ChetRippo <bdavis1000@gmail.com>
2020-05-01 18:19:59 -0400

    fix string casing

--- src/js/npm/jws/Verifier.hx
+++ src/js/npm/jws/Verifier.hx
@@ -4,5 +4,5 @@ import js.npm.jwk.*;
 
 @:jsRequire("node-jose", "Verifier")
 extern class Verifier {
-  function verify(input: string):js.lib.Promise<Dynamic>;
+  function verify(input: String):js.lib.Promise<Dynamic>;
 }
